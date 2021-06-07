be8451ea369c2833b795a48bf8e84454ac2e22e9
Grigorios Georgolopoulos <53451691+ggeorgol@users.noreply.github.com>
2021-06-07 10:11:44 +0200

    Update hypergeo_motif_enrichment.sh

--- hypergeo_motif_enrichment.sh
+++ hypergeo_motif_enrichment.sh
@@ -6,14 +6,13 @@ module load R/3.6.1
 
 TEMPDIR=./tmp_motif_enrichment/
 
-mkdir $TEMPDIR
+mkdir -p $TEMPDIR
 
-INPUT=$1
-MOTIFS=$2
+REGIONS=$1
+REF=$2
+MOTIFS=$3
 
-awk -F'\t' '{print $1 FS $2 FS $3 FS FNR FS $4;}' $INPUT > $TEMPDIR/annotated_input.bed
-
-NF=`awk '{print NF; exit}' $INPUT`
+NF=`awk '{print NF; exit}' $REGIONS`
 
 if (("$NF" < "4")); then
 	echo "A BED file with 4 columns is required" 1>&2
@@ -21,16 +20,31 @@ if (("$NF" < "4")); then
 	exit 1
 fi
 
+if [ "$REF" == "rest" ];
+then
+	echo 'Compare enchrichment against' $REF
+else
+	if grep -Rq $REF $REGIONS;
+	then
+		echo 'Compare enchrichment against' $REF
+	else
+		echo 'Please select one of:' `cut -f 4 $REGIONS | sort | uniq | tr '\n' '\ '`
+		exit 1
+	fi
+fi
+
+awk -F'\t' '{print $1 FS $2 FS $3 FS FNR FS $4;}' $REGIONS > $TEMPDIR/regions.bed
+
 echo $(date +"%Y-%m-%d %T %Z") "Mapping motifs to query"
 
-cut -f 1-3 $INPUT | sort-bed - | tabix -R - $MOTIFS | sort-bed - > $TEMPDIR/motifs_mapped_to_query.txt
+cut -f 1-3 $REGIONS | sort-bed - | tabix -R - $MOTIFS | sort-bed - > $TEMPDIR/motifs_mapped_to_query.txt
 
 echo $(date +"%Y-%m-%d %T %Z") "Assigning DHS index to mapped motifs"
 
-cut -f 1-4 $TEMPDIR/motifs_mapped_to_query.txt | bedmap --ec --echo --echo-map-id --skip-unmapped --delim '\t' - $TEMPDIR/annotated_input.bed > $TEMPDIR/input_index_to_motifs.txt
+cut -f 1-4 $TEMPDIR/motifs_mapped_to_query.txt | bedmap --ec --echo --echo-map-id --skip-unmapped --delim '\t' - $TEMPDIR/regions.bed > $TEMPDIR/motifs_to_region_index.txt
 
-RINPUT=$TEMPDIR/input_index_to_motifs.txt
+RINPUT=$TEMPDIR/motifs_to_region_index.txt
 
-Rscript /home/ggeorgol/scripts/hypergeo_motif_enrichment.R $RINPUT $TEMPDIR/annotated_input.bed
+Rscript ./hypergeo_motif_enrichment.R $RINPUT $TEMPDIR/regions.bed $REF
 
 #rm -r $TEMPDIR
