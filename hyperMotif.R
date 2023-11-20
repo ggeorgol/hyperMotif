b5ec219563859f2c6eecfc24e7594ae328f75d5c
Grigorios Georgolopoulos <ggeorgol@ggeorgol-air.router.wind>
2023-11-20 09:21:28 +0200

    updated scripts

---
+++ hyperMotif.R
@@ -0,0 +1,122 @@
+#!/usr/bin/env Rscript
+
+pkgs = c('data.table','Matrix', 'tidyverse')
+
+for (pkg in pkgs) {
+
+	if (!require(pkg, character.only = TRUE)) {
+    print(sprintf("Installing package %s", pkg))
+	install.packages(pkg)
+
+	suppressMessages(library(pkg, character.only = TRUE))
+    
+	} else {
+        suppressMessages(library(pkg, character.only = TRUE))
+	}
+
+}
+
+args = commandArgs(trailingOnly = TRUE)
+
+options(scipen = 4)
+
+message(paste0(Sys.time(),':',' Reading motif matches\n'))
+
+master = fread(args[1], sep = '\t', stringsAsFactors = F, data.table = F)
+outfile = args[4]
+
+#### Fix overlapping matches ####
+
+#if (sum(grepl('\\;', master$V5)>0)) {
+#    oo = master[grep('\\;',master$V5),5]
+#    oo = oo[seq(1, length(oo),by = 2)]
+#    oo = unlist(strsplit(oo,split = '[;]'))
+#    master[grep('\\;',master$V5),5] = oo
+#}
+
+message(paste0(Sys.time(),':',' Separating motifs overlapping multiple peaks\n'))
+
+master <- master %>% 
+mutate(id = paste(V1,V2,V3,V4)) %>% 
+filter(!duplicated(id)) %>% 
+separate_rows(V5,sep = ';') 
+
+motifs = unique(sort(master$V4))
+
+input=fread(args[2], sep = '\t', stringsAsFactors = F, data.table = F, select = 1:5)
+
+groups = unique(sort(input$V5))
+
+ref_group = args[3]
+
+
+################################################
+#####        Calculate counts matrix       #####
+################################################
+
+message(paste0(Sys.time(),':',' Computing DHS by Motif count matrix\n'))
+
+motif_mat = Matrix::Matrix(table(master[,c(5,4)]), sparse = T)
+
+################################################
+##### Calculate frequency and fold-changes #####
+################################################
+
+results = data.frame()
+
+for(group in groups[!groups %in% ref_group]) {
+
+    message(paste0(Sys.time(),':',' Computing frequency and fold-changes for group ',group,'\n'))
+    
+    test_regions = input$V4[ input$V5 == group ]
+    test_regions = intersect(master$V5, test_regions)
+
+    if (ref_group == 'rest') {
+
+    	ref_regions = input$V4[ input$V5 != group ]
+
+    } else {
+
+    	ref_regions = input$V4[ input$V5 == ref_group ]
+
+    }
+
+    ref_regions = intersect(master$V5, ref_regions)
+    
+    query = t(motif_mat[test_regions, ])
+    ref = t(motif_mat[ref_regions, ])
+    
+    freqs_query = rowSums(query)/sum(rowSums(query))
+    freq_ref = rowSums(ref)/sum(rowSums(ref))
+    
+    diff_freq = log2(freqs_query)-log2(freq_ref)
+
+    freq = freqs_query[motifs]
+    q = rowSums(query[motifs,]) # number of white balls drawn
+    m = q + rowSums(ref[motifs,]) # total number of white balls
+    n = sum(rowSums(query)) + sum(rowSums(ref)) - m # total number of black balls
+    k = sum(rowSums(query)) # number of balls drawn
+    
+    p = ifelse(diff_freq > 0, 
+              phyper(q-1, m, n, k, lower.tail = FALSE),
+              phyper(q, m, n, k, lower.tail = TRUE))
+    
+    tmp = data.frame("motif_name" = motifs, 
+                     "l2fc" = diff_freq, 
+                     "freq" = freq, 
+                     "p_val" = p, 
+                     "fdr" = NA, 
+                     "motif_counts_in_test" = q, 
+                     "motif_counts_in_reference" = m - q, 
+                     "total_motifs_in_test" = k, 
+                     "contrast" = paste0(group,'_vs_',ref_group))
+    
+    results = rbind(results, tmp)
+
+}
+
+results$fdr = p.adjust(results$p_val, method = 'BH')
+results = results[order(results$contrast, -results$l2fc),]
+
+#print(results)
+write.table(results, paste0(outfile,'/enrichment_results.txt'), col.names = TRUE, row.names = FALSE, quote = FALSE, sep = '\t')
---
+++ hyperMotif.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+#module load htslib
+#module load bedops
+#module load R/3.6.1
+
+if [[ $# == 0 ]]; then
+  echo "No arguments were provided. Exiting."
+  echo "	Usage:"
+  echo "	-b  A 4-column BED file where 4th column is the group assignment"
+  echo "	-r  The reference group. Should be one of the available groups or set to 'rest' \nto compare against the rest of the groups"
+  echo "	-m  The motif scans file"
+  echo "	-o  The output directory"
+  exit -1
+fi
+
+while getopts ":b:r:m:o:" opt; do
+  case $opt in
+    b) REGIONS="$OPTARG"
+    ;;
+    r) REF="$OPTARG"
+    ;;
+    m) MOTIFS="$OPTARG"
+    ;;
+    o) OUTDIR="$OPTARG"
+    ;; 
+    \?) echo "Invalid option -$OPTARG" >&2
+echo "	Usage:"
+echo "	-b  A 4-column BED file where 4th column is the group assignment"
+echo "	-r  The reference group. Should be one of the available groups or set to 'rest' to compare against the rest of the groups"
+echo "	-m  The motif scans file"
+echo "	-o  The output directory"
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+TEMPDIR=$OUTDIR/"tmp_motif_enrichment/"
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+SCRIPTPARENT=`dirname $SCRIPTPATH`
+
+#REGIONS=$1
+#REF=$2
+#MOTIFS=$3
+#OUTFILE=$4
+
+if [[ ! -f "${REGIONS}" ]]; then
+    echo "Regions BED file not found."
+exit 1
+fi
+
+if [[ ! -f "${MOTIFS}" ]]; then
+    echo "Motif scans file not found."
+exit 1
+fi
+
+if [[ ! -d $OUTDIR ]]; then
+mkdir -p $OUTDIR
+fi
+
+mkdir -p $TEMPDIR
+
+NF=`awk '{print NF; exit}' $REGIONS`
+
+if (("$NF" < "4")); then
+	echo "A BED file with 4 columns is required" 1>&2
+
+	exit 1
+fi
+
+NROW=$(wc -l "${REGIONS}" | cut -d ' ' -f 1)
+echo "$NROW regions detected"
+cut -f 4 $REGIONS | sort | uniq -c
+
+if [ "$REF" == "rest" ];
+then
+	echo $(date +"%Y-%m-%d %T %Z") 'Testing enchrichment against' $REF
+else
+	if grep -Rq $REF $REGIONS;
+	then
+		echo $(date +"%Y-%m-%d %T %Z") 'Testing enchrichment against' $REF
+	else
+		echo 'Please select one of:' `cut -f 4 $REGIONS | sort | uniq | tr '\n' '\ '`1>&2
+		exit 1
+	fi
+fi
+
+awk -F'\t' '{print $1 FS $2 FS $3 FS "region"FNR FS $4;}' $REGIONS | sort-bed - > $TEMPDIR/regions.bed
+
+echo $(date +"%Y-%m-%d %T %Z") "Mapping motifs to query"
+
+cut -f 1-3 $REGIONS | sort-bed - | tabix -R - $MOTIFS | sort-bed - > $TEMPDIR/motifs_mapped_to_query.txt
+#tabix -R $TEMPDIR/regions.bed $MOTIFS | sort-bed - > $TEMPDIR/motifs_mapped_to_query.txt
+
+echo $(date +"%Y-%m-%d %T %Z") "Assigning DHS index to mapped motifs"
+
+cut -f 1-4 $TEMPDIR/motifs_mapped_to_query.txt | bedmap --ec --echo --echo-map-id --skip-unmapped --delim '\t' - $TEMPDIR/regions.bed > $TEMPDIR/motifs_to_region_index.txt
+
+RINPUT=$TEMPDIR/motifs_to_region_index.txt
+
+Rscript $SCRIPTPARENT/r/hypergeo_motif_enrichment.R $RINPUT $TEMPDIR/regions.bed $REF $OUTDIR
+
+#rm -r $TEMPDIR
+
